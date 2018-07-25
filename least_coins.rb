ae4fca5ff5c7aecc997dfe1cebaaa56832cb7523
vaalcat <val.cat456@icloud.com>
2018-07-25 18:52:14 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,10 +3,11 @@
 def least_coins(cents)
 least_coins = (50)
   solution= {}
-  num_dimes = cents/1 
   quarters = cents/ 25
   
   solution[:quarters]= num_quarters 
+  
+  
   
   puts solution 
 
